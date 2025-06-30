7d7bbe84ce5de340a69f5ff4007ecd7ab643dd0d
Aaditya Panda <aadityapanda23@gmail.com>
2025-06-30 11:34:54 +0000

    no this isnt much better. it still looks the same

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,7 +18,7 @@ const buttonVariants = cva(
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80 hover:-translate-y-1 shadow-lg hover:shadow-xl",
         github:
-          "bg-[#333] text-primary-foreground hover:bg-black/90 shadow-lg hover:shadow-xl hover:shadow-gray-900/20 hover:-translate-y-1",
+          "bg-[#333] text-white hover:bg-black/90 shadow-lg hover:shadow-xl hover:shadow-gray-900/20 hover:-translate-y-1",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
